9f0e59fff8f33647277b927e7ec381f454a5c451
saadsiddiqui07 <saadsiddiqui927@gmail.com>
2025-12-05 21:19:46 +0530

    add stock details components

--- app/stock-dashboard.tsx
+++ app/stock-dashboard.tsx
@@ -1,8 +1,10 @@
 import GlobalLayout from "@/components/global-layout";
 import StockHeader from "@/components/stock-dashboard/Header";
+import { ThemedText } from "@/components/themed-text";
 import AnimatedCounter from "@/components/ui/animated-counter";
+import { IconSymbol } from "@/components/ui/icon-symbol.ios";
 import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 
 const StockDashboard = () => {
   const [value, setValue] = useState<number>(24908.15);
@@ -18,7 +20,66 @@ const StockDashboard = () => {
   return (
     <GlobalLayout style={styles.container}>
       <StockHeader />
-      <AnimatedCounter value={value} fontSize={80} variant="large" />
+      <View
+        style={{
+          marginTop: 28,
+          paddingHorizontal: 16,
+          flexDirection: "column",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "flex-start",
+            justifyContent: "space-between",
+          }}
+        >
+          {/* left side */}
+          <View style={{ flex: 1, gap: 16 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 32,
+              }}
+            >
+              <Pressable style={styles.iconButton}>
+                <IconSymbol name="apple.logo" color="lightgray" size={30} />
+              </Pressable>
+              <View style={{ gap: 4 }}>
+                <ThemedText type="subtitle">APPL</ThemedText>
+                <ThemedText style={{ fontSize: 14, color: "gray" }}>
+                  Apple Inc.
+                </ThemedText>
+              </View>
+            </View>
+            <AnimatedCounter value={value} fontSize={35} variant="large" />
+          </View>
+
+          {/* right side */}
+          <View
+            style={{
+              borderRadius: 12,
+              backgroundColor: "rgba(255, 255, 255, 0.1)",
+            }}
+          >
+            <View style={{ gap: 4, padding: 4 }}>
+              <Pressable style={styles.rightIcon}>
+                <IconSymbol name="chart.bar.xaxis" color="white" size={24} />
+              </Pressable>
+              <Pressable
+                style={[styles.rightIcon, { backgroundColor: "lightblue" }]}
+              >
+                <IconSymbol
+                  name="chart.line.uptrend.xyaxis"
+                  color="black"
+                  size={24}
+                />
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </View>
     </GlobalLayout>
   );
 };
@@ -26,7 +87,21 @@ const StockDashboard = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
+    backgroundColor: "rgb(24, 24, 24)",
+  },
+  iconButton: {
+    padding: 16,
+    borderRadius: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  rightIcon: {
+    padding: 16,
+    borderRadius: 16,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
